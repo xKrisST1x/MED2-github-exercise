217bfe1ebeb75ffe6798553c2d7fb30a272d06f3
malteerasmussen <malteerasmussen@gmail.com>
2022-02-16 10:04:07 +0100

    Changed Unity version to 2020.3.28f1

---
+++ Github Unity Exercise/Assets/ExerciseCode.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ExerciseCode : MonoBehaviour
+{
+    int myVariable = 0;
+
+    private void Start() {
+        Debug.Log("value before calling function: " + myVariable);
+        
+        // Call the function 'PassByValue' below this line – pass 'myVariable' as argument.
+
+        
+        Debug.Log("value after calling function: " + myVariable);
+    }
+
+    public void PassByValue(int var)
+    {
+        var = var + 10;
+        Debug.Log("Value inside function: " + var);
+    }
+
+    public void PassByReference(ref int var)
+    {
+        var = var + 20;
+        Debug.Log("Value inside function: " + var);
+    }
+    
+}
